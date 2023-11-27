3829e45a9b4ed4d2566933b1e65782da3f1e8b94
Enes Talha Uçar <talhauenes@gmail.com>
2023-11-27 16:40:56 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Enes Talha Uçar  on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
